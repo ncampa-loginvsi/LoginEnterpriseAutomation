b07a15a2e3bb3e0f7320d08727f9248c70ac72fa
Nick <n.campa@loginvsi.com>
2022-12-09 15:08:52 -0500

    Add Greeting, Logic based Test creation

---
+++ POC/Scripts/PovSetup.ps1
@@ -0,0 +1,99 @@
+Param(
+    [string]$Fqdn,
+    [string]$Token,
+    [string]$FilePath = ".\POC\Resources\Accounts.csv",
+    [string]$ConnectorType,
+    [string]$TargetRDPHost,
+    [string]$ServerUrl,
+    [string]$TargetResource,
+    [string]$Debug
+)
+
+$global:Fqdn = $Fqdn
+$global:Token = $Token
+
+function Main {
+    param (
+        $FilePath,
+        $ConnectorType,
+        $TargetRDPHost,
+        $ServerUrl,
+        $TargetResource,
+        $Debug
+    )
+
+    Write-Host `n
+    Write-Host "Welcome to the Login Enterprise Proof-Of-Value Setup tool. This tool will assist with getting you off the ground, and straight to finding the value..." -ForegroundColor "Cyan"
+    Write-Host "Developed in December 2022 by Nick Campa for Login VSI..." -ForegroundColor "Cyan"
+    Write-Host `n
+
+    # Import functions from wd
+    Write-Host "Start: Beginning function import process..." -ForegroundColor "White"
+    . .\POC\Scripts\API.ps1
+    Write-Host "[DEBUG] API Endpoint commands successfully imported..." -ForegroundColor "Green"
+    . .\POC\Scripts\Helpers.ps1
+    Write-Host "[DEBUG] API Helper functions successfully imported..." -ForegroundColor "Green"
+
+    # Confirm connection parameters
+    Greet-User -ConnectorType $ConnectorType -TargetRDPHost $TargetRDPHost -ServerUrl $ServerUrl -TargetResource $TargetResource
+  
+    # Import .csv file of accounts into the appliance
+    Write-Host "[ACCOUNTS] Start: Beginning account import process..." -ForegroundColor "White"
+    $AccountIds = Import-LeAccounts -FilePath $FilePath
+    Write-Host "[ACCOUNTS] End: Account import process completed..." -ForegroundColor "White"
+
+    # Create empty account groups to add users to
+    Write-Host "[GROUPS] Start: Beginning account groups creation process..." -ForegroundColor "White"
+    $AccountGroupIds = Import-AccountGroups
+    Write-Host "[GROUPS] End: Account groups have been created..." -ForegroundColor "White"
+
+    # Create empty launcher group to add launchers to
+    # In 4.10 this will no longer be needed, should be a default group for new installations
+    Write-Host "[GROUPS] Start: Beginning launcher group creation process..." -ForegroundColor "White"
+    $LauncherGroupId = Import-LeLauncherGroup -LauncherGroupName "All Launchers" -Description "This is a group containing all launchers."
+    Write-Host "[GROUPS] End: Launcher groups have been created..." -ForegroundColor "White"
+
+    # AppId, LoadId, ContId
+    Write-Host "[CLEANUP] Attempting to create tests..." -ForegroundColor "White"
+    $TestIds = Import-Tests -AccountGroupId $AccountGroupIds -LauncherGroupId $LauncherGroupId -ConnectorType $ConnectorType -TargetRDPHost $TargetRDPHost -ServerUrl $ServerUrl -TargetResource $TargetResource
+    Write-Host "[TESTS] End: Tests have been created..." -ForegroundColor "White"
+   
+
+    if ($Debug -eq "Y") {
+        Write-Host "[CLEANUP] Start: Beginning cleanup process..." -ForegroundColor "White"
+        Debug-Cleanup -AccountIds $AccountIds -AccountGroupIds $AccountGroupIds -LauncherGroupId $LauncherGroupId -TestIds $TestIds
+        Write-Host "[CLEANUP] End: Cleanup process has been completed..." -ForegroundColor "White"
+    } 
+    
+    Write-Host "[DEBUG] Script has completed. Enjoy your proof of concept..." -ForegroundColor "Green"
+    
+}
+
+# Pass non-sensitive arguments here, and call it from the command line: ".\ProofOfValue.ps1 -Fqdn <YOUR_FQDN> -Token <YOUR_SECRET_TOKEN>"
+Main -FilePath $FilePath -ConnectorType $ConnectorType -TargetRDPHost $TargetRDPHost -ServerUrl $ServerUrl -TargetResource $TargetResource -Debug $Debug
+
+
+#[string]$Fqdn,
+#[string]$Token,
+#[string]$FilePath,
+#[string]$ConnectorType,
+#[string]$Target # For now this will be either RDP host or Storefront URL
+
+# CreateTest("Application Test", type="appTest")
+# CreateTest("Capacity Baseline", type="loadTest")
+# CreateTest("Hunting for Failure", type="contTest")
+
+# Create Three tests:
+# Create an Application Test (PowerPoint, Word, Excel, Notepad, Paint)
+# Create a Load Test (PowerPoint, Word, Excel, Notepad, Paint)
+# Create a Continuous Test (PowerPoint, Word, Excel, Notepad, Paint)
+
+# Add three locations:
+# Create new location 1
+# Create new location 2
+# Create new location 3
+
+
+# READD THESE
+#chrisMoltisanti,Password2,newark.nj
+#paulieWalnuts,Password3,newark.nj
